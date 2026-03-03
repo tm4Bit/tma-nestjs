9a12305d5fbd1bde36106005284d6764ac6901d5
Tiago Araujo <tiagomuniz130@gmail.com>
2026-03-02 22:26:38 -0300

    refactor: remove old code

--- src/validation/zod-validation.ts
+++
@@ -1,5 +0,0 @@
-import { ZodValidationPipe } from 'nestjs-zod';
-
-export const zodBodyValidationPipe = new ZodValidationPipe();
-export const zodQueryValidationPipe = new ZodValidationPipe();
-export const zodParamsValidationPipe = new ZodValidationPipe();
