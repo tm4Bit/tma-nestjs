784d49adae84499668ab1ccc9c1036770d678410
Tiago Araujo <tiagomuniz130@gmail.com>
2026-03-02 22:34:08 -0300

    chore: add comment to document code

--- src/blog-posts/blog-posts.scheduler.ts
+++ src/blog-posts/blog-posts.scheduler.ts
@@ -1,11 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Interval } from '@nestjs/schedule';
+// import { Interval } from '@nestjs/schedule';
 
 @Injectable()
 export class BlogPostsScheduler {
   private readonly logger = new Logger(BlogPostsScheduler.name);
 
-  @Interval(15000)
+  //////////////////////////////////////////////////////////////////////////////
+  // NOTE: Uncomment the following line and the import statement
+  // to test the scheduler functionality.
+  // This method will log a heartbeat message every 15 seconds.
+  //////////////////////////////////////////////////////////////////////////////
+  // @Interval(15000)
   logHeartbeat(): void {
     this.logger.log('blog-posts heartbeat');
   }
